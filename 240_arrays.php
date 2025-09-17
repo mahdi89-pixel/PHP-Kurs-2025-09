745f78fd39e8a5fe7a618973016d51dc8c50fbfa
mahdi89-pixel <vejdani.mahdi89@gmail.com>
2025-09-17 16:30:54 +0200

    Start

---
+++ 240_arrays.php
@@ -0,0 +1,47 @@
+<?php
+
+//arrays
+
+
+// Nummerisches Array (in Python die Liste)
+
+$leute =['Peter', 'Paul', 'Mary'];
+
+//echo $leute; //Array & Warning
+
+//Zur Information beim Programmieren:
+var_dump($leute);
+
+echo $leute[0]. "\n";
+echo $leute[1]. "\n";
+echo $leute[2]. "\n";
+
+//Assoziative Array(in Python das Dictionary)
+
+$person =[
+    'vorname' => 'Peter',
+    'nachname' => 'Weller',
+    'alter' => 39
+];
+
+var_dump($person);
+
+echo $person['vorname']. "\n";
+echo $person['nachname']. "\n";
+echo $person['alter']. "\n";
+
+//count()
+//gibt die Anzahl der Indexe/Keys zurück.
+echo count($leute) . "\n"; //3
+echo count($person) . "\n";//3
+
+//foreach-schleife (speziell für Arrays)
+
+foreach ($leute as $leut){
+    echo 'hallo'. ' ' . $leut . "\n";
+}
+
+
+foreach ($person as $s => $w){ //$s für Schlüssel, $w für Wert
+    echo $s . ': ' . $w . "\n";
+}
