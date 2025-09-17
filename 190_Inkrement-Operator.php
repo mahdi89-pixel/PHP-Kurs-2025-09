abdd07529e312bd130168ff4e29e8e0b917b1878
mahdi89-pixel <vejdani.mahdi89@gmail.com>
2025-09-17 11:00:38 +0200

    Start

---
+++ 190_Inkrement-Operator.php
@@ -0,0 +1,22 @@
+<?php
+// Inkrementoperator
+
+$zahl1 = 0;
+
+$zahl1 = $zahl1 + 1;
+
+print $zahl1 . '<br>'; //1
+
+$zahl1 += 1;
+
+print $zahl1 . '<br>'; //2
+
+$zahl1++;
+
+// Der Inkrement-Operator erhöht um 1.
+print $zahl1 . '<br>'; //3
+
+// Der Dekrement-Operator
+
+$zahl1--;
+print $zahl1;
---
+++ 200_for-Schleife.php
@@ -0,0 +1,23 @@
+<?php
+//for schleife
+
+//Zählerschleife
+//(Initialisierung ; Bedingung, Veränderung)
+
+//Initialisierung
+
+//Bedingung
+//Schleifenkörper
+//Veränderung
+
+//Bedingung
+//Schleifenkörper
+//Veränderung
+
+//...
+
+for($i = 1; $i <= 3; $i++){
+    print'Hi <br>';
+}
+
+// for (;;) {} <-> while (true)
