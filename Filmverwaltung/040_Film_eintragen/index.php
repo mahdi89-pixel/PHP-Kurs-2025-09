4febdb4effd660fa739923f20f9ce17ecddaa374
mahdi89-pixel <vejdani.mahdi89@gmail.com>
2025-09-23 13:55:15 +0200

    ja

---
+++ Filmverwaltung/040_Film_eintragen/index.php
@@ -0,0 +1,89 @@
+<?php
+
+// require_once bindet die angegebene Datei an genau der Stelle im Quellcode ein.
+require_once 'lib/db_verbindung.php';
+
+$stmt = $db->query('SELECT id, titel, jahr, laenge, genre  FROM filme;');
+$filme = $stmt->fetchAll();
+
+if (empty($filme)) {
+    echo "<p>Keine Filme gefunden.</p>";
+    exit;
+}
+//var_dump($filme);
+
+var_dump($filme[0]);  // array(9)
+
+//echo $filme[0]['titel'];  // Equalizer
+
+$headings = array_keys($filme[0]);
+// Spaltenüberschriften entfernen:
+unset($headings[0]);  // In PHP werden Arrays nicht neu indiziert
+unset($headings[8]);
+//var_dump($headings);
+
+
+foreach ($headings as $k => $v) {
+    //if ($v == 'fsk') $v = strtoupper($v);
+    if ($v == 'laenge') $v = 'länge';
+    $headings[$k] = $v;
+}
+
+
+
+$headings = array_map('ucfirst', $headings);
+
+foreach ($filme as $key => $film) {
+    // unset($film['id']);     // Inhalte entfernen
+    unset($film['cover']);  // Inhalte entfernen
+    $filme[$key] = $film;
+}
+
+?>
+
+<!-- TODO: Fsk -> FSK -->
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Filme anzeigen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Filme anzeigen</h1>
+
+<table>
+
+    <tr>
+        <?php foreach ($headings as $heading) { ?>
+            <th>
+                <?php echo $heading; ?>
+            </th>
+        <?php } ?>
+    </tr>
+
+    <?php foreach ($filme as $film) { ?>
+
+        <tr onclick="location.href='film_anzeigen.php?id=<?php echo $film['id']; ?>'">
+            <?php foreach ($film as $k => $v) { ?>
+
+
+                <?php
+                if ($k != 'id') echo "<td>$v</td>";
+                ?>
+
+
+            <?php } ?>
+        </tr>
+
+    <?php } ?>
+
+
+
+</table>
+
+
+
+</body>
+</html>
