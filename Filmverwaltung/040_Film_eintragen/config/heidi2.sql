2d3d0e6bacc4ee2828e727372cbba44d1737bfcf
mahdi89-pixel <vejdani.mahdi89@gmail.com>
2025-09-23 15:23:11 +0200

    ja

--- Filmverwaltung/040_Film_eintragen/config/heidi2.sql
+++ Filmverwaltung/040_Film_eintragen/config/heidi2.sql
@@ -1,4 +1,10 @@
 
-USE filmverwaltung;
+USE  filmverwaltung;
 
-SELECT * FROM filme;
+SELECT *FROM filme;
+
+SELECT *FROM filme WHERE id =1;
+
+-- INSERT INTO filme (titel, jahr, genre, vertrieb, fsk, einspielergebnis, laenge) ...
+
+
